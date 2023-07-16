62c5572f54cb51114dc82adc1936a90b9ec87027
isonguyom <isonguyom@gmail.com>
2023-07-16 15:34:27 +0100

    first commit

---
+++ server/index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import path from "path";
+import bodyParser from "body-parser";
+import multer from "multer";
+const upload = multer();
+import homepageRouter from "./homepageRouter.js";
+import todos from "./todos.js";
+import assetsRouter from "./assetsRouter.js";
+
+const port = process.env.PORT || 3000;
+const publicPath = path.join(path.resolve(), "public");
+const distPath = path.join(path.resolve(), "dist");
+
+const app = express();
+
+app.get("/api/v1/hello", (_req, res) => {
+  res.json({ message: "Hello, world!" });
+});
+
+// var todos = require('./todos.js');
+
+// //Use the Router on the sub route /movies
+// app.use('/api', todos);
+
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(distPath));
+} else {
+  app.use("/", express.static(publicPath));
+  app.use("/src", assetsRouter);
+}
+
+// for parsing application/json
+app.use(bodyParser.json());
+
+// for parsing multipart/form-data
+app.use(upload.array());
+
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/api/todos", todos);
+app.use(homepageRouter);
+
+app.listen(port, () => {
+  console.log("Server listening on port", port);
+});
---
+++ src/components/CreateTodo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <form action="" @submit.prevent="$emit('create-todo')" method="post">
+      <input
+        type="text"
+        name="newTodo"
+        id="newTodo"
+        placeholder="Enter todo"
+        :value="newTodo"
+        @input="$emit('update:newTodo', $event.target.value)"
+      />
+      <button type="submit">Create todo</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ["create-todo", "update:newTodo"],
+  props: ["newTodo"],
+};
+</script>
